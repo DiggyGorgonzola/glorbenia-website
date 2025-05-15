62c04640173a409eaf5c5d8cbbbedc07c9851425
Diggy Gorgonzola <164104182+DiggyGorgonzola@users.noreply.github.com>
2025-05-15 13:20:30 -0500

    Update __init__.py
    Added the 4 html pages we have

--- glorbenia_site/__init__.py
+++ glorbenia_site/__init__.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
+def home():
+  return render_template("index.html")
+@app.route('/index.html')
 def index():
   return render_template("index.html")
-  
+@app.route('/webstates.html')
+def webstates():
+  return render_template("webstates.html")
+@app.route('/updates.html')
+def updates():
+  return render_template("updates.html")
+@app.route('/pagenotfound.html')
+def pagenotfound():
+  return render_template("pagenotfound.html")
